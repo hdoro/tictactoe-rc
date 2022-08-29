a7736c836ea93d3c50c360da2ee771f34cd796c0
Henrique Doro <opensource@hdoro.dev>
2022-08-29 15:06:39 -0300

    refactor: clarify game state logic

--- src/gameState.js
+++ src/gameState.js
@@ -22,21 +22,21 @@ export const STATE_RETURNS = {
  *
  * @returns "ongoing" | "draw" | "O-wins" | "X-wins"
  */
-export function validateState(gameState) {
-  if (!Array.isArray(gameState)) {
+export function validateState(gameRows) {
+  if (!Array.isArray(gameRows)) {
     throw new Error(STATE_ERRORS.nonArray);
   }
-  if (gameState.length !== 3) {
+  if (gameRows.length !== 3) {
     throw new Error(STATE_ERRORS.rowCount);
   }
-  if (gameState.some((row) => row.length !== 3)) {
+  if (gameRows.some((row) => row.length !== 3)) {
     throw new Error(STATE_ERRORS.columnCount);
   }
-  if (gameState.flat().some((cell) => ![1, 0, null].includes(cell))) {
+  if (gameRows.flat().some((cell) => ![1, 0, null].includes(cell))) {
     throw new Error(STATE_ERRORS.invalidCell);
   }
 
-  const valueCount = gameState.reduce((count, curRow) => {
+  const valueCount = gameRows.reduce((count, curRow) => {
     return {
       X: (count["X"] || 0) + curRow.filter((cell) => cell === 1).length,
       O: (count["O"] || 0) + curRow.filter((cell) => cell === 0).length,
@@ -53,39 +53,31 @@ export function validateState(gameState) {
   }
 
   // Switch from rows to columns before we can analyze column wins
-  const columns = gameState.reduce((cols, row) => {
+  const columns = gameRows.reduce((cols, row) => {
     return row.map((cell, index) => [...(cols[index] || []), cell]);
   }, []);
 
   // Switch from rows to diagonals before we can analyze diagonals wins
   const diagonals = [
     // 1st index in 1st row -> last index in last row
-    [gameState[0][0], gameState[1][1], gameState[2][2]],
+    [gameRows[0][0], gameRows[1][1], gameRows[2][2]],
 
     // Last index in 1st row -> 1st index in last row
-    [gameState[2][0], gameState[1][1], gameState[0][2]],
+    [gameRows[2][0], gameRows[1][1], gameRows[0][2]],
   ];
 
-  console.log({ diagonals });
-  const winState = [
-    ...gameState, // row wins
+  const allCellSequences = [
+    ...gameRows, // row wins
     ...columns, // column wins
     ...diagonals, // diagonal wins
-  ]
-    .map((sequence) => {
-      // If the sequence starts with a number, let's check if it's fully made out of that number
-      if (Number.isInteger(sequence[0])) {
-        // If we find any cell that is different from the first cell in the sequence, we don't have a win state
-        return sequence.some((cell) => cell !== sequence[0])
-          ? undefined
-          : // Else, return the proper win state for the given number
-            STATE_RETURNS[sequence[0]];
-      }
-    })
-    .find((state) => !!state);
+  ];
 
-  if (winState) {
-    return winState;
+  const winnerSequence = allCellSequences.find((sequence) =>
+    cellSequenceHasWin(sequence)
+  );
+
+  if (winnerSequence) {
+    return cellSequenceHasWin(winnerSequence);
   }
 
   // If no player has won, return draw ONYL if valueCount.empty === 0
@@ -95,4 +87,15 @@ export function validateState(gameState) {
 
   // Otherwise, game still running
   return "ongoing";
+}
+
+function cellSequenceHasWin(sequence) {
+  // If the sequence starts with a number, let's check if it's fully made out of that number
+  if (Number.isInteger(sequence[0])) {
+    // If we find any cell that is different from the first cell in the sequence, we don't have a win state
+    return sequence.some((cell) => cell !== sequence[0])
+      ? undefined
+      : // Else, return the proper win state for the given number
+        STATE_RETURNS[sequence[0]];
+  }
 }
